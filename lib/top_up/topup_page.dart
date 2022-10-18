d73732f8b6bbdc3c751562c9b15954bf114171a9
tyndalesanjay <60053448+tyndalesanjay@users.noreply.github.com>
2022-10-18 00:18:01 -0500

    made update

--- lib/top_up/topup_page.dart
+++ lib/top_up/topup_page.dart
@@ -102,7 +102,7 @@ class _TopUpState extends State<TopUp> {
             style: ButtonStyle(
                 backgroundColor: MaterialStateProperty.all(Colors.amber), padding: MaterialStatePropertyAll(EdgeInsets.only(left: 30, right: 30))),
             onPressed: () {},
-            child: Text('Submit', style: TextStyle(fontSize: 20),),
+            child: Text('Submit', style: TextStyle(fontSize: 20, color: Colors.black),),
           ),
         )
       ]),
